76b35c0c9b6f5fe617b683bb4aff011c6980f558
zseaborn63 <zseaborn63@gmail.com>
2016-02-17 15:33:15 -0500

    Program is working

--- divisors.py
+++ divisors.py
@@ -1,1 +1,9 @@
-print("hello world")
+print("Welcome to Number Construction Extractor!")
+user_num = int(input("What is your number?\n"))
+
+divisors = []
+for x in range(1, user_num):
+    if user_num % x == 0:
+        divisors.append(x)
+
+print(divisors)
